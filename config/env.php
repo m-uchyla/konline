ad867bf09ce5a2cca4bec3433a32b414559785d4
VEEX16 <45610643+VEEX16@users.noreply.github.com>
2020-04-30 19:06:36 +0200

    Update env.php

--- config/env.php
+++ config/env.php
@@ -24,3 +24,6 @@ $settings['db']['host']     = 'ec2-54-228-251-117.eu-west-1.compute.amazonaws.co
 $settings['db']['database'] = 'd68i0vtk27v33j';
 $settings['db']['username'] = 'jsezzwkxwhkidz';
 $settings['db']['password'] = 'eea4bd40f58ba0ced65bcbc74a8e15492ec94284982e1b446ff0b4f7e3b0ec1e';
+$settings['db']['port'] = 5432;
+$settings['db']['URI'] = 'postgres://jsezzwkxwhkidz:eea4bd40f58ba0ced65bcbc74a8e15492ec94284982e1b446ff0b4f7e3b0ec1e@ec2-54-228-251-117.eu-west-1.compute.amazonaws.com:5432/d68i0vtk27v33j'
+$settings['db']['CLI'] = 'heroku pg:psql postgresql-cubed-33102 --app konline'
